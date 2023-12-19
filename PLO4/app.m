7a13d7586fae42357f06204adb708965b4905d5c
GulhermeC <gjscraveiro@ua.pt>
2023-12-19 16:58:33 +0000

    update

--- PLO4/app.m
+++ PLO4/app.m
@@ -1,12 +1,12 @@
 clc;
 clear all;
 
-load database; % allGenres genreCounts
+load database; % allGenres genreCounts genreYearCounts
 
 op = 0;
-menu(op, allGenres, genreCounts);
+menu(op, allGenres, genreCounts, genreYearCounts);
 
-function menu(op, genres, genreCounts)
+function menu(op, genres, genreCounts, genreYearCounts)
     while(isempty(op) | op < 6)
         fprintf('\n1 - Display available genres\n2 - Number of movies of a genre\n3 - Number of a genre on a given year\n4 - Search movie titles\n5 - Search movies based on genres\n6 - Exit\nSelect an option: ')
         op = str2num(input('', 's'));
@@ -22,7 +22,7 @@ function menu(op, genres, genreCounts)
             case 2
                 moviesGen(genreCounts)
             case 3
-                moviesGenYr()
+                moviesGenYr(genreYearCounts)
             case 4
                 searchMovie()
             case 5
@@ -46,21 +46,68 @@ end
 
 % OP 2
 function moviesGen(genreCounts)
+
+    % Number of elements expected to add
+    n = numel(genreCounts.keys);
+    % Probability of false positives
+    p = 0.01;
+
+    % Size of the bit array
+    m = ceil((n * log(p)) / log(1 / (2 ^ log(2))));
+    % Number of hash functions
+    k = round((m / n) * log(2));
+
+    % Initialize Bloom filter (bit array)
+    bloomFilter = false(1, m);
+
+    % Populate Bloom filter
+    genres = genreCounts.keys;
+    for i = 1:length(genres)
+        for j = 1:k
+            % Using DJB31MA with varying seeds
+            hash = DJB31MA(genres{i}, j);
+            bloomIndex = mod(hash, m) + 1;
+            bloomFilter(bloomIndex) = true;
+        end
+    end
+
     fprintf('Select a genre: ');
     gen = input('', 's');
-    if isKey(genreCounts, gen)
-        fprintf('The genre "%s" is part of the keys.\n', gen);
-        count = genreCounts(gen);
-        fprintf('The genre "%s" has %d movies.\n', gen, count);
+
+    % Check if the genre is possibly in the Bloom filter
+    isPossible = true;
+    for j = 1:k
+        hash = DJB31MA(gen, j);
+        bloomIndex = mod(hash, m) + 1;
+        if ~bloomFilter(bloomIndex)
+            isPossible = false;
+            break;
+        end
+    end
+
+    if isPossible
+        if isKey(genreCounts, gen)
+            count = genreCounts(gen);
+            fprintf('The genre "%s" has %d movies.\n', gen, count);
+        end
     else
         fprintf('The genre "%s" is not part of the keys.\n', gen);
     end
 end
 
 % OP 3
-function moviesGenYr()
+function moviesGenYr(genreYearCounts)
     fprintf("Select a genre and a year (separated by ','): ");
     in = input('', 's');
+    
+    inputParts = strsplit(in, ',');
+
+    % Check if the input format is correct
+    if length(inputParts) == 2
+        % Extract genre and year
+        gen = strtrim(inputParts{1});
+        yr = strtrim(inputParts{2});
+    end
 end
 
 % OP 4
--- PLO4/database.m
+++ PLO4/database.m
@@ -9,11 +9,14 @@ allGenres = {};
 % Initialize a containers.Map to store genre counts
 genreCounts = containers.Map('KeyType', 'char', 'ValueType', 'int32');
 
+% Initialize a nested containers.Map to store genre-year counts
+genreYearCounts = containers.Map('KeyType', 'char', 'ValueType', 'any');
+
 % Loop through each movie entry
 for i = 1:size(movies, 1)
     % Extract genres for this movie
     % Assuming genres start from column 3 till the end of the columns
-    year = movies{i, 2};
+    year = char(movies{i, 2});
     genres = movies(i, 3:end);
     
     % Remove any empty cells or NaN values
@@ -25,20 +28,40 @@ for i = 1:size(movies, 1)
         % Ensure genre is a string and not missing
         if ~ismissing(genres{j})
             genre = char(genres{j});
+
             % Check if the genre is already in allGenres
             if ~ismember(genre, allGenres)
                 % Append to the allGenres array
                 allGenres{end + 1} = genre;
             end
+
+            % =================== 2 =================== 
+
             % Update the count for each genre
             if isKey(genreCounts, genre)
                 genreCounts(genre) = genreCounts(genre) + 1;
             else
                 genreCounts(genre) = 1;
             end
+
+            % =================== 3 ===================
+
+            % Check if the genre is already in genreYearCounts
+            if ~isKey(genreYearCounts, genre)
+                genreYearCounts(genre) = containers.Map('KeyType', 'char', 'ValueType', 'int32');
+            end
+
+            % Update the count for each genre-year
+            yearMap = genreYearCounts(genre);
+            if isKey(yearMap, year)
+                yearMap(year) = yearMap(year) + 1;
+            else
+                yearMap(year) = 1;
+            end
+            genreYearCounts(genre) = yearMap;
         end
     end
 
 end
 
-save 'database' 'allGenres' 'genreCounts'
+save 'database' 'allGenres' 'genreCounts' 'genreYearCounts'
